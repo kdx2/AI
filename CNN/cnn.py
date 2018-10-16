cc0f58f3ffad48cd95708f86f1e4917e0d87d0b9
Constantine Dimitrov <konydim@gmail.com>
2018-10-16 10:05:35 +0100

    ADD: New Hidden Layer & more
    Now there are two hidden layers. Second one has half the number neurons of the first one. The batch size is increased. Added another conv layer. Accuracy now reaches 85%.

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -12,6 +12,7 @@ from keras.layers import Convolution2D, MaxPooling2D, Flatten
 from keras.layers import Dense, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import model_from_json
+import h5py
 
 # Initialize the CNN
 classifier = Sequential()
@@ -19,9 +20,14 @@ classifier = Sequential()
 # Define the convolutional layer. Create 32 feature
 # detectors for a 32 feature maps. Activation function to make sure
 # no negative values are present in the image.
-classifier.add(Convolution2D(32, 3, 3, input_shape=(32, 32, 3), activation='relu'))
+classifier.add(Convolution2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
 # Pooling. Do on the max-pooling basis
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# Add a second convolutional layer
+# It's a common strategy to double the number of feature detectors. 
+classifier.add(Convolution2D(20, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
 # Flattening
@@ -29,7 +35,12 @@ classifier.add(Flatten())
 
 # The FC-ANN. The hidden layer with added 60% dropout. 
 classifier.add(Dense(128, activation='relu', use_bias=True))
-classifier.add(Dropout(rate=0.6)) 
+classifier.add(Dropout(rate=0.75))
+
+# A second hidden layer
+classifier.add(Dense(64, activation='relu', use_bias=True))
+classifier.add(Dropout(rate=0.8))
+
 # Output layer. Sigmoid, because it's a squashing function between
 # 0 and 1. Perfect for binary predictions like the current one.
 classifier.add(Dense(1, activation='sigmoid'))
@@ -52,19 +63,22 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
-        target_size=(32, 32),
-        batch_size=32,
+        target_size=(64, 64),
+        batch_size=64,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
-        target_size=(32, 32),
+        target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 
 classifier.fit_generator(
         training_set,
         steps_per_epoch=8000, # Number of images in the training set
-        epochs=20,
+        epochs=26,
         validation_data=test_set,
         validation_steps=2000) # Number images of out test set.
+
+# Save only the weights of the model
+classifier.save_weights('weightsCatDogCNN.hdf5')
